dd72079cb8df776615f789a8e90c0c2c589e4a4b
Chien Le <lchien063@gmail.com>
2023-09-06 11:55:31 +0700

    feat: add asm model

---
+++ models/staging/gSheet/base/base_gSheet__asms.sql
@@ -0,0 +1,18 @@
+WITH source AS (
+    SELECT
+        *,
+    FROM
+        {{ source(
+            'gSheet',
+            '_ext_asm_list'
+        ) }}
+)
+SELECT
+    asm,
+    store_name,
+    phone,
+    email
+FROM
+    source
+WHERE
+    asm <> 'ASM'
---
+++ models/staging/gSheet/stg_gsheet__asms.sql
@@ -0,0 +1,29 @@
+{{config(materialized='table')}}
+
+WITH asm_list AS (
+    SELECT
+        asm AS asm_name,
+        CASE
+            WHEN store_name = '5S Thái Bình 1' THEN '5S Thái bình 1'
+            WHEN store_name LIKE '5S Thanh Hoá%' THEN REGEXP_REPLACE(
+                store_name,
+                'Thanh Hoá',
+                'Thanh Hóa'
+            )
+            ELSE store_name
+        END AS store_name,
+        phone,
+        email,
+    FROM
+        {{ ref('base_gSheet__asms') }}
+)
+SELECT
+    asm_list.asm_name,
+    branches.branch_id,
+    asm_list.phone,
+    asm_list.email,
+FROM
+    asm_list
+    LEFT JOIN {{ ref('stg_kiotviet__branches') }}
+    branches
+    ON asm_list.store_name = branches.branch_name
