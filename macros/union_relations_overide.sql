dc8b415ff6c7ebba7e0f4e2aebc363346611135e
Chien Le <lchien063@gmail.com>
2023-11-17 17:32:13 +0700

    feat: add deduplication and union_relations

---
+++ macros/union_relations_overide.sql
@@ -0,0 +1,37 @@
+{% macro union_relations(
+        relations,
+        exclude,
+        include
+    ) %}
+    {% if target.name != 'prod' %}
+        {% set overide_union %}
+        {% for relation in relations %}
+        SELECT
+            {% if include is defined %}
+                {{ include |
+                JOIN(',') }}
+            {% else %}
+                * {% if exclude is defined %}
+                EXCEPT
+                    (
+                        {{ exclude |
+                        JOIN(',') }}
+                    )
+                {% endif %}
+            {% endif %}
+        FROM
+            {{ relation }}
+            {{ 'union all' if not loop.last }}
+        {% endfor %}
+
+        {% endset %}
+        {% do return(overide_union) %}
+    {% else %}
+        {% set dbt_utils_unions = dbt_utils.union_relations(
+            relations = relations,
+            exclude = exclude,
+            include = include
+        ) %}
+        {% do return(dbt_utils_unions) %}
+    {% endif %}
+{% endmacro %}
