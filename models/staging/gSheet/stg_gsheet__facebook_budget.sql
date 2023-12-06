10fe75c09b3dcaffaa18fd1f012b0ac0f834a931
Chien Le <lchien063@gmail.com>
2023-12-06 15:48:21 +0700

    feat: facebook_budget model

--- models/staging/gSheet/stg_gsheet__facebook_budget.sql
+++ models/staging/gSheet/stg_gsheet__facebook_budget.sql
@@ -2,17 +2,20 @@
     materialized = 'table',
     tags = ['dimension','table', 'ignore']
 ) }}
-{%- set relation = source('gSheet','_ext_facebook_budget') -%}
 
+{%- set relation = source(
+    'gSheet',
+    'facebook_budget'
+) -%}
 {% set all_columns = adapter.get_columns_in_relation(relation) %}
-{% set targets = { "budget":"ngân sách",
-"sales_target":"doanh số",
-"traffic_target":"lượt khách" } %}
-
+{% set targets ={ "budget" :"ngân sách",
+"sales_target" :"doanh số",
+"traffic_target" :"lượt khách",
+"aov" :'gttb/kh' } %}
 WITH source AS (
 
     SELECT
-        *,
+        * except(`ASM - Showroom`),
         ROW_NUMBER() over (
             PARTITION BY showroom,
             MONTH
@@ -22,56 +25,55 @@ WITH source AS (
     FROM
         {{ source(
             'gSheet',
-            '_ext_facebook_budget'
+            'facebook_budget'
         ) }}
     WHERE
         MONTH >= '2023-11-01'
 ),
-formated as 
-(SELECT
-    case 
-        when source.showroom = '5S Hải Dương' then '5S Hải Dương 1'
-        when source.showroom = '5S Thái Nguyên 1' then '5S Thái Nguyên'
-        when source.showroom = '5S GO' then '5S Go Thái Bình'
-        when source.showroom = '5S Hà Tĩnh 1' then '5S Hà Tĩnh' 
-        else source.showroom
-    end AS branch,
-    source.month AS budget_month,
-    {% for col in all_columns %}
-        {% for key,value in targets.items()  %}
-            {% if value in col.name.lower() %}
-                source.`{{col.name}}` AS {{key}},
-            {% endif %}
-        {% endfor %}
-    {% endfor %}
-    ARRAY_AGG(
-        STRUCT(
-            milestones.start,
-            milestones.end,
-        milestones.value,
-        regexp_extract(
-            milestones.key,
-            r'^(.*) -'
-        ) AS milestone_name,
+formated AS (
+    SELECT
         CASE
-            LOWER(regexp_extract(milestones.key, r'- ([^()]+)$')) 
-            {% for key, value in targets.items() %}
-                WHEN '{{ value }}' THEN '{{ key }}'
-            {% endfor %}
-        END AS budget_type
-    )
-) as milestones
+            WHEN source.showroom = '5S Hải Dương' THEN '5S Hải Dương 1'
+            WHEN source.showroom = '5S Thái Nguyên 1' THEN '5S Thái Nguyên'
+            WHEN source.showroom = '5S GO' THEN '5S Go Thái Bình'
+            WHEN source.showroom = '5S Hà Tĩnh 1' THEN '5S Hà Tĩnh'
+            ELSE source.showroom
+        END AS branch,
+        source.month AS budget_month,
+        {# source.target, #}
+        ARRAY_AGG(
+            STRUCT(
+                milestones.start,
+                milestones.
+            END,
+            milestones.value,
+            regexp_extract(
+                milestones.key,
+                r'^(.*) -'
+            ) AS milestone_name,
+            CASE
+                LOWER(regexp_extract(milestones.key, r'- ([^()]+)$')) 
+                {% for key,value in targets.items() %}
+                    WHEN '{{ value }}' THEN '{{ key }}'
+                {% endfor %}
+            END AS budget_type
+        )
+) AS milestones
 FROM
-   source,
+    source,
     unnest(milestones) milestones
 WHERE
-    rn_ = 1
-{{dbt_utils.group_by(5)}}
+    rn_ = 1 {{ dbt_utils.group_by(2) }}
 )
-
-select formated.*,
-branch.branch_id,
-asm.page,
-from formated
-left join {{ref('stg_kiotviet__branches')}} branch on formated.branch = branch.branch_name
-left join {{ref('stg_gsheet__asms')}} asm on branch.branch_id = asm.branch_id
+SELECT
+    formated.*,
+    branch.branch_id,
+    asm.page,
+FROM
+    formated
+    LEFT JOIN {{ ref('stg_kiotviet__branches') }}
+    branch
+    ON formated.branch = branch.branch_name
+    LEFT JOIN {{ ref('stg_gsheet__asms') }}
+    asm
+    ON branch.branch_id = asm.branch_id
