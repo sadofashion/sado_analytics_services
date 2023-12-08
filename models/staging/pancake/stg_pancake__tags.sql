7e81a01e6fad39adf92c85e8d6281afda1cbd0e7
Chien Le <lchien063@gmail.com>
2023-12-08 16:43:08 +0700

    fix: pancake staging models

--- models/staging/pancake/stg_pancake__tags.sql
+++ models/staging/pancake/stg_pancake__tags.sql
@@ -2,7 +2,7 @@ WITH source AS (
     {{ dbt_utils.deduplicate(relation = source('pancake', 'tags'), partition_by = 'id', order_by = "_batched_at desc",) }}
 )
 SELECT
-source.id as tag_id,
-source.text as tag_value,
+    source.id AS tag_id,
+    source.text AS tag_value,
 FROM
     source
