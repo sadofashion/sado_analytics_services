f5c51e13f0769f9fad14125a31f502acbed03c0a
Chien Le <lchien063@gmail.com>
2023-12-09 15:07:29 +0700

    fix: add tags config from gsheet

---
+++ models/staging/gSheet/stg_gsheet__pancake_tags.sql
@@ -0,0 +1,12 @@
+{{ config(
+    materialized = 'table',
+    tags = ['gsheet','dimension','table', 'pancake']
+) }}
+
+SELECT
+    tag_value, category
+FROM
+    {{ source(
+        'gSheet',
+        _ext_pancake_tags
+    ) }}
