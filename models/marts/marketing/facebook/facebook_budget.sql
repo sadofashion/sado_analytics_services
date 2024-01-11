f7dbb6d6f69290107fd95f178370024fbb9092e0
Chien Le <lchien063@gmail.com>
2024-01-11 17:06:30 +0700

    -f

--- models/marts/marketing/facebook/facebook_budget.sql
+++ models/marts/marketing/facebook/facebook_budget.sql
@@ -1,5 +1,12 @@
 {{ config(
-    tags = ['table', 'fact','budget','ignore']
+    materialized = 'incremental',
+    partition_by ={ 'field': 'date',
+    'data_type': 'date',
+    'granularity': 'month' },
+    unique_key = ['branch_id','date'],
+    incremental_strategy = 'merge',
+    on_schema_change = 'sync_all_columns',
+    tags = ['incremental', 'fact','budget','ignore']
 ) }}
 
 {% set targets = ["budget", "sales_target", "traffic_target",'aov'] %}
@@ -9,7 +16,8 @@ WITH processed AS (
         *
     EXCEPT
         ({% for item in targets %}
-            val_{{ item }} {{ ", " if not loop.last}}
+            val_{{ item }}
+            {{ ", " if not loop.last }}
         {% endfor %}),
         {% for item in targets %}
             safe_divide(val_{{ item }}, date_diff(tb.END, tb.start, DAY) + 1) AS daily_{{ item }},
@@ -29,28 +37,26 @@ FROM
 
         #}
         FROM
-            {{ ref('stg_gsheet__facebook_budget') }}
-            b,
+            {{ ref('stg_gsheet__facebook_budget') }} b,
             unnest(milestones) milestones
-    ) pivot (SUM(VALUE) AS val for budget_type IN (
+    ) 
+    pivot (
+    SUM(VALUE) AS val for budget_type IN (
         {% for item in targets %}
-        '{{item}}' {{ ", " if not loop.last}}
-    {% endfor %}
-    )) AS tb
+            '{{item}}' {{ ", " if not loop.last }}
+        {% endfor %}
+    )
+    ) AS tb
 )
 SELECT
     processed.*,
     C.date,
-    {# {% for metric in metrics %%}
-    SUM(
-        fb.{{ metric }}
-    ) {{ metric }},
-{% endfor % %}
-#}
 FROM
     processed
-    CROSS JOIN {{ ref("calendar") }} C {# left join {{ref("facebook_performance")}} fp on processed.page = fp.page and c.date = fp.date_start #}
+    CROSS JOIN {{ ref("calendar") }} C 
 WHERE
     C.date >= processed.start
-    AND C.date <= processed.
-END {# {{dbt_utils.group_by(14)}} #}
+    AND C.date <= processed.end {# {{dbt_utils.group_by(14)}} #}
+        {% if is_incremental() %}
+          and processed.start >=  date_add(date(_dbt_max_partition))
+        {% endif %}
--- models/staging/5sfashion/base/base_5sfashion__provinces.sql
+++ models/staging/5sfashion/base/base_5sfashion__provinces.sql
@@ -1,12 +1,18 @@
 WITH source AS (
-    {{dbt_utils.deduplicate(source=source(
+    {{ dbt_utils.deduplicate(
+        relation = source(
             '5sfashion',
             'provinces'
-        ),partition_by='_id', order_by='updated_at DESC,_batched_at DESC'
-        )}}
+        ),
+        partition_by = '_id',
+        order_by = 'updated_at DESC,_batched_at DESC'
+    ) }}
 )
 SELECT
     *
-EXCEPT(rn_, _batched_at)
+EXCEPT(
+        rn_,
+        _batched_at
+    )
 FROM
     source
