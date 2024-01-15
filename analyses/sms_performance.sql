199ef3963d08f3d2040ecd46be38afb9f13f7f80
Chien Le <lchien063@gmail.com>
2024-01-15 15:53:00 +0700

    -f

--- analyses/sms_performance.sql
+++ analyses/sms_performance.sql
@@ -1,9 +1,13 @@
-{{ config(
-    materialized = 'incremental',
-    unique_key = 'phone',
-) }}
 
-WITH sms AS (
+
+WITH rfm_ as (
+    select customer_id,start_of_month, segment,
+    coalesce(lag(segment) over (partition by customer_id order by start_of_month asc),'First-time Purchaser') prev_segment
+    from {{ ref('rfm_movement') }}
+    where start_of_month >='2022-12-01'
+),
+
+sms AS (
 
     SELECT
         DISTINCT s.phone,
@@ -12,14 +16,14 @@ WITH sms AS (
             s.sent_time
         ) sent_time,
         r.segment,
+        r.prev_segment,
         s.sms_cost,
     FROM
-        {{ ref("stg_esms__sent_data") }}
+        {{ ref('stg_esms__sent_data') }}
         s
-        LEFT JOIN {{ ref("stg_kiotviet__customers") }} C
+        LEFT JOIN {{ ref('stg_kiotviet__customers') }} C
         ON s.phone = C.contact_number
-        LEFT JOIN {{ ref("rfm_movement") }}
-        r
+        LEFT JOIN rfm_ r
         ON C.customer_id = r.customer_id
         AND DATE_TRUNC(
             s.sent_time,
@@ -40,6 +44,7 @@ revenue AS (
         {{ ref('revenue') }}
     WHERE
         transaction_type = 'invoice'
+        and transaction_date>='2023-01-01'
     GROUP BY
         1,
         2
@@ -64,19 +69,15 @@ performance as (
             sms.sent_time,
             DAY
         ) <= 7
-    {# GROUP BY
-        1,
-        2,
-        3,
-        4 #}
+    
 )
 SELECT
-    segment,
-    days_lag,
+    prev_segment,
+    avg(days_lag) days_lag,
     SUM(num_invoice) num_invoice,
     SUM(total_invoice_value) total_invoice_value,
     count(phone) as num_sent,
     sum(sms_cost) sms_cost,
 FROM
     performance
-group by 1,2
+group by 1
