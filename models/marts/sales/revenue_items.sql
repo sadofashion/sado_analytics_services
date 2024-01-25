aadcd73be65c1b4a2f92161f303c042aa6aa73ee
Chien Le <lchien063@gmail.com>
2024-01-25 14:01:44 +0700

    fix: revenue_items to merge strategy

--- models/marts/sales/revenue_items.sql
+++ models/marts/sales/revenue_items.sql
@@ -4,8 +4,8 @@
     partition_by ={ 'field': 'transaction_date',
     'data_type': 'timestamp',
     'granularity': 'day' },
-    incremental_strategy = 'insert_overwrite',
-    unique_key = ['transaction_id','product_id'],
+    incremental_strategy = 'merge',
+    unique_key = ['transaction_id','product_id','price'],
     on_schema_change = 'sync_all_columns'
 ) }}
 
@@ -22,31 +22,33 @@ SELECT
     invoices.customer_id,
     invoices.product_id,
     invoices.product_code,
-    invoices.quantity,
     invoices.price,
-    COALESCE(
+    invoices.transaction_type,
+    sum(invoices.quantity) quantity,
+    avg(COALESCE(
         invoices.discount_ratio,
         safe_divide(
             invoices.discount * 100,
             invoices.price
         ),
         0
-    ) discount_ratio,
-    invoices.discount,
-    invoices.subTotal,
-    invoices.transaction_type,
+    )) discount_ratio,
+    avg(invoices.discount) discount,
+    sum(invoices.subTotal) subTotal,
 FROM
     {{ ref('stg_kiotviet__invoicedetails') }}
     invoices
 WHERE
     invoices.transaction_status = 'Hoàn thành'
     AND invoices.quantity <> 0
-
 {% if is_incremental() %}
 AND (date(coalesce(invoices.modified_date, invoices.transaction_date)) >= DATE(_dbt_max_partition)
 OR date(coalesce(invoices.modified_date, invoices.transaction_date)) >= date_sub(CURRENT_DATE(), INTERVAL 2 DAY))
 {% endif %}
+{{dbt_utils.group_by(12)}}
+
 UNION ALL
+
 SELECT
     returns.transaction_id,
     returns.transaction_code,
@@ -58,16 +60,16 @@ SELECT
     returns.customer_id,
     returns.product_id,
     returns.product_code,
-    returns.quantity,
     returns.price,
+    returns.transaction_type,
+    sum(returns.quantity) quantity, 
     CAST(
         NULL AS float64
     ) AS discount_ratio,
     CAST(
         NULL AS float64
     ) AS discount,
-    returns.subTotal,
-    returns.transaction_type
+    sum(returns.subTotal) subTotal,
 FROM
     {{ ref('stg_kiotviet__returndetails') }}
     returns
@@ -78,3 +80,4 @@ WHERE
 AND (date(coalesce(returns.modified_date, returns.transaction_date)) >= DATE(_dbt_max_partition)
 OR date(coalesce(returns.modified_date, returns.transaction_date)) >= date_sub(CURRENT_DATE(), INTERVAL 2 DAY))
 {% endif %}
+{{dbt_utils.group_by(12)}}
