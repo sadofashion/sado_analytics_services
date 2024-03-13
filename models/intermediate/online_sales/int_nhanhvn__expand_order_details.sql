d5a2dc98c52ff831788887d59b40ad19bb0d87c4
Chien Le <lchien063@gmail.com>
2024-03-13 17:13:24 +0700

    feat: add aggregation in intermediate model orders_items

--- models/intermediate/online_sales/int_nhanhvn__expand_order_details.sql
+++ models/intermediate/online_sales/int_nhanhvn__expand_order_details.sql
@@ -3,9 +3,10 @@ SELECT
     products.product_id,
     products.product_code,
     products.price,
-    products.quantity,
-    products.item_discount,
+    sum(products.quantity) quantity,
+    sum(products.item_discount) item_discount,
 FROM
     {{ ref('stg_nhanhvn__ordersdetails') }}
     orders,
     unnest(products) products
+    {{dbt_utils.group_by(40)}}
