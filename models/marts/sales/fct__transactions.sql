a45b7dc5f48222010f646f368d0d955744ccc77e
Chien Le <lchien063@gmail.com>
2024-05-14 11:21:39 +0700

    fix: online sales incremental logic [skip actions]

--- models/marts/sales/fct__transactions.sql
+++ models/marts/sales/fct__transactions.sql
@@ -148,9 +148,9 @@ nhanhvn_rev AS (
     WHERE
         1 = 1
 {% if is_incremental() %}
-and DATE(created_date) in (
+and DATE(delivery_date) in (
     select 
-    DATE(created_date)
+    DATE(delivery_date)
     from {{ ref('stg_nhanhvn__ordersdetails') }}
     where date(last_sync) >= date_add(CURRENT_DATE, INTERVAL -1 DAY)
 )
