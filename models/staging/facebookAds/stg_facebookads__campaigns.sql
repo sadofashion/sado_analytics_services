15e7dc1f65651641a5d2ccbe0d4ff3bd1af75e28
Chien Le <lchien063@gmail.com>
2024-05-29 10:55:55 +0700

    feat: dimension seeds

--- models/staging/facebookAds/stg_facebookads__campaigns.sql
+++ models/staging/facebookAds/stg_facebookads__campaigns.sql
@@ -1,76 +1,59 @@
 {{ config(
     materialized = 'view',
-    tags = ['fb','dimension','table']
+    tags = ['fb','dimension','view']
 ) }}
 
-WITH source AS (
+WITH 
+old_convention as (
+    old_naming
+),
+current_campaign_name AS (
 
     SELECT
         DISTINCT account_id,
-        FIRST_VALUE (account_name) over (
-            PARTITION BY account_id
-            ORDER BY
-                _batched_at DESC
-        ) AS account_name,
-        FIRST_VALUE (campaign_name) over (
-            PARTITION BY campaign_id
-            ORDER BY
-                _batched_at DESC
-        ) AS campaign_name,
-        MIN(date_start) over (
-            PARTITION BY campaign_id
-        ) AS campaign_start_date,
-        MAX(date_start) over (
-            PARTITION BY campaign_id
-        ) AS campaign_stop_date,
+        FIRST_VALUE (account_name) over (PARTITION BY account_id ORDER BY _batched_at DESC) AS account_name,
+        upper(FIRST_VALUE (campaign_name) over ( PARTITION BY campaign_id ORDER BY _batched_at DESC )) AS campaign_name,
+        upper(FIRST_VALUE (adset_name) over (PARTITION BY adset_id ORDER BY _batched_at DESC )) AS adset_name,
+        upper(FIRST_VALUE (ad_name) over (PARTITION BY ad_id ORDER BY _batched_at DESC)) AS ad_name,
+        MIN(date_start) over (PARTITION BY campaign_id) AS campaign_start_date,
+        MAX(date_start) over (PARTITION BY campaign_id) AS campaign_stop_date,
         campaign_id
     FROM
         {{ source(
             'facebookAds',
             'p_AdsInsights__*'
         ) }}
-)
-SELECT
-    *,
-    regexp_extract (
-        campaign_name,
-        r"^(?:.*?_){4}(.*?)_(?:.*?)$"
-    ) AS big_campaign,
+        where date_start >= '2024-06-01'
+),
+convention_version as (
+    select * 
+    case when campaign_name like '5S%' then 'B2406' else '2406' end as convention_version
+    from current_campaign_name
+),
 
-    regexp_extract (
-        campaign_name,
-        r"^(?:.*?_){4}(.*?_.*?)_(?:.*?)$"
-    ) AS content_group,
+new_naming_convention as (
+    select 
+        * ,
+        {{parse_naming_convention(campaign_col = "campaign_name", adset_col = "adset_name", ad_col = "ad_name")}}
+    from convention_version
+    where convention_version = '2406'
+),
+old_naming_convention(
+    SELECT
+        *,
+        regexp_extract (campaign_name,r"^(?:.*?_){4}(.*?)_(?:.*?)$") AS big_campaign,
+        regexp_extract (campaign_name,r"^(?:.*?_){4}(.*?_.*?)_(?:.*?)$") AS content_group,
+        regexp_extract (campaign_name,r"^(?:.*?_){1}(.*?)_(?:.*?)$") AS pic,
+        regexp_extract (campaign_name,r"^(?:.*?_){6}(.*?)_(?:.*?)$") AS promoted_productline,
+        regexp_extract (campaign_name,r"^(.*?)_") AS page,
+        regexp_extract (campaign_name,r"(?:.*?_){7}(.*?)_(?:.*?)") AS media_type,
+        regexp_extract (campaign_name,r"^(?:.*?_){2}(.*?)_(?:.*?)$") AS funnel,
+        regexp_extract (campaign_name,r"^(?:.*?_){3}(.*?)_(?:.*?)$") AS ad_type,
+    from convention_version
+    where convention_version = 'B2406'
+    )
 
-    regexp_extract (
-        campaign_name,
-        r"^(?:.*?_){1}(.*?)_(?:.*?)$"
-    ) AS pic,
 
-    regexp_extract (
-        campaign_name,
-        r"^(?:.*?_){6}(.*?)_(?:.*?)$"
-    ) AS promoted_productline,
 
-    regexp_extract (
-        campaign_name,
-        r"^(.*?)_"
-    ) AS page,
-
-    regexp_extract (
-        campaign_name,
-        r"(?:.*?_){7}(.*?)_(?:.*?)"
-    ) AS media_type,
-
-    regexp_extract (
-        campaign_name,
-        r"^(?:.*?_){2}(.*?)_(?:.*?)$"
-    ) AS funnel,
-
-    regexp_extract (
-        campaign_name,
-        r"^(?:.*?_){3}(.*?)_(?:.*?)$"
-    ) AS ad_type,
-
-FROM
-    source
+select * 
+from new_naming_convention
--- models/staging/gSheet/stg_gsheet__asms.sql
+++ models/staging/gSheet/stg_gsheet__asms.sql
@@ -3,28 +3,22 @@
     tags = ['gsheet','dimension','table']
 ) }}
 
-{# WITH asm_list AS ( #}
-
-    SELECT
-        asm AS asm_name,
-        CASE
-            WHEN store_name = '5S Thái Bình 1' THEN '5S Thái bình 1'
-            WHEN store_name LIKE '5S Thanh Hoá%' THEN REGEXP_REPLACE(
-                store_name,
-                'Thanh Hoá',
-                'Thanh Hóa'
-            )
-            ELSE store_name
-        END AS store_name,
-        phone,
-        email,
-        local_page,
-        region_page,
-        pic,
-        opening_day,
-        close_date,
-        region,
-        province,
-
-    FROM
-        {{ ref('base_gSheet__asms') }}
+SELECT
+    asm AS asm_name,
+    CASE
+        WHEN store_name = '5S Thái Bình 1' THEN '5S Thái bình 1'
+        WHEN store_name LIKE '5S Thanh Hoá%' THEN REGEXP_REPLACE(
+            store_name,
+            'Thanh Hoá',
+            'Thanh Hóa'
+        )
+        ELSE store_name
+    END AS store_name,
+    * except(store_name,asm)
+FROM
+    {{ source(
+        'gSheet',
+        '_ext_asm_list'
+    ) }}
+WHERE
+    asm <> "ASM"
