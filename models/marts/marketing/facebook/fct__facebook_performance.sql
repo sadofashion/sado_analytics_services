8ab349d787ee3c63abcfd8af267c7cac55c67f4e
Chien Le <lchien063@gmail.com>
2024-05-29 21:55:25 +0700

    feat: facebook performance model

--- models/marts/marketing/facebook/fct__facebook_performance.sql
+++ models/marts/marketing/facebook/fct__facebook_performance.sql
@@ -17,79 +17,24 @@ WITH source AS (
         impressions,
         reach,
         spend,
+        {% for metric in metrics %}
+            (
+            SELECT
+                actions.value
+            FROM
+                unnest (actions) actions 
+            WHERE actions.action_type = '{{metric}}'
+            ) AS num_{{metric | replace('.','_')}},
 
-        (
-            SELECT
-                actions.value
-            FROM
-                unnest (actions) actions
-            WHERE
-                actions.action_type = 'link_click'
-        ) AS link_click,
-        (
-            SELECT
-                actions.value
-            FROM
-                unnest (actions) actions
-            WHERE
-                actions.action_type = 'post_engagement'
-        ) AS post_engagement,
-        (
-            SELECT
-                actions.value
-            FROM
-                unnest (actions) actions
-            WHERE
-                actions.action_type = 'onsite_conversion.messaging_conversation_started_7d'
-        ) AS messaging_conversation_started_7d,
-        (
-            SELECT
-                actions.value
-            FROM
-                unnest (actions) actions
-            WHERE
-                actions.action_type = 'offline_conversion.purchase'
-        ) AS offline_conversion_purchase,
-        (
+            (
             SELECT
                 action_values.value
             FROM
                 unnest (action_values) action_values
             WHERE
-                action_values.action_type = 'offline_conversion.purchase'
-        ) AS offline_conversion_purchase_value,
-        (
-            SELECT
-                actions.value
-            FROM
-                unnest (actions) actions
-            WHERE
-                actions.action_type = 'offsite_conversion.fb_pixel_purchase'
-        ) AS pixel_purchase,
-        (
-            SELECT
-                action_values.value
-            FROM
-                unnest (action_values) action_values
-            WHERE
-                action_values.action_type = 'offsite_conversion.fb_pixel_purchase'
-        ) AS pixel_purchase_value,
-        (
-            SELECT
-                actions.value
-            FROM
-                unnest (actions) actions
-            WHERE
-                actions.action_type = 'onsite_conversion.purchase'
-        ) AS meta_purchase,
-        (
-            SELECT
-                action_values.value
-            FROM
-                unnest (action_values) action_values
-            WHERE
-                action_values.action_type = 'onsite_conversion.purchase'
-        ) AS meta_purchase_value
+                action_values.action_type = '{{metric}}'
+            ) as {{metric | replace('.','_') }}_value,
+        {% endfor %}
     FROM
         {{ ref('stg_facebookads__adsinsights') }}
 )
