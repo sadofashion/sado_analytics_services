5e7b043bde8554cc28d8a5b3ba53a7b729801561
Chien Le <lchien063@gmail.com>
2024-06-25 17:36:49 +0700

    feat: fb insights dimensions && metrics

--- models/marts/marketing/facebook/fct__page_impressions_by_city.sql
+++ models/marts/marketing/facebook/fct__page_impressions_by_city.sql
@@ -4,6 +4,8 @@
 ) }}
 
 SELECT
-    *
+    * except(city),
+    trim(SPLIT(city," - ")[safe_offset(1)]) AS country,
+    trim(SPLIT(city," - ")[safe_offset(0)]) AS city,
 FROM
     {{ ref("fct_fb__page_impressions_by_city") }}
--- models/marts/marketing/facebook/fct__video_view_time_by_city.sql
+++ models/marts/marketing/facebook/fct__video_view_time_by_city.sql
@@ -4,7 +4,9 @@
 ) }}
 
 SELECT
-    *,
+    * except(city),
     split(post_id,"_")[0] as page_id,
+    trim(SPLIT(city," - ")[safe_offset(1)]) AS country,
+    trim(SPLIT(city," - ")[safe_offset(0)]) AS city,
 FROM
     {{ ref("fct_fb__video_view_time_by_city") }}
