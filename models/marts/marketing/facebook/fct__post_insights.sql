b5e902a5d17f9e5ccc461212bebd2ce6cbe2117e
Chien Le <lchien063@gmail.com>
2024-06-26 15:01:09 +0700

    fix: metric_time 1day lagged

--- models/marts/marketing/facebook/fct__post_insights.sql
+++ models/marts/marketing/facebook/fct__post_insights.sql
@@ -4,7 +4,8 @@
 ) }}
 
 SELECT
-    *, 
-    split(post_id,"_")[0] as page_id,
+    * except(metric_time), 
+    split(post_id,"_")[safe_offset(0)] as page_id,
+    date_add(metric_time, interval 1 day) as metric_time,
 FROM
     {{ ref("fct_fb__post_insights") }}
