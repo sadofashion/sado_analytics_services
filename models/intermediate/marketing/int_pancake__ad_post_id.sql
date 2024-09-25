1d43556d1c5a889789b989af5e515f7d879242c1
Chien Le <lchien063@gmail.com>
2024-09-25 11:01:28 +0700

    fix: campaigns

--- models/intermediate/marketing/int_pancake__ad_post_id.sql
+++ models/intermediate/marketing/int_pancake__ad_post_id.sql
@@ -1,5 +1,5 @@
 select
-    split(ad_post_id)[offset(0)] as ad_page_id,
+    split(ad_post_id,'_')[offset(0)] as ad_page_id,
     ad_id, 
     {# {{dbt_utils.dbt_utils.generate_surrogate_key('ad_post_id', 'ad_id')}} as ad_post_id_surrogate_key #}
 from {{ ref("base_pancake__conversations") }}
