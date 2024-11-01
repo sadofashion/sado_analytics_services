6ec35872fe04256c337e07fa18eaf1984c4cd88b
Chien Le <lchien063@gmail.com>
2024-11-01 16:32:58 +0700

    feat: fix class_code

--- models/marts/dimensions/dim__inventory_classes.sql
+++ models/marts/dimensions/dim__inventory_classes.sql
@@ -6,14 +6,14 @@
 }}
 select 
 distinct
-class_code,
+product_design_code as class_code,
 first_value(sub_productline ignore nulls) over w1 as sub_productline,
 first_value(productline ignore nulls) over w1 as productline,
 first_value(category ignore nulls) over w1 as category,
 first_value(product_group ignore nulls) over w1 as product_group,
 first_value(season ignore nulls) over w1 as season
 from {{ ref("dim__inventory_items") }}
-where class_code is not null
+where product_design_code is not null
 and (category not in ('WEBSITE (Không xóa)') or category is null)
-window w1 as (partition by class_code order by kiotviet_product_id rows BETWEEN unbounded preceding and unbounded following)
+window w1 as (partition by product_design_code order by kiotviet_product_id rows BETWEEN unbounded preceding and unbounded following)
 {# and kiotviet_product_id is not null #}
--- models/marts/online_sales/fct_sp__orders.sql
+++ models/marts/online_sales/fct_sp__orders.sql
@@ -34,6 +34,7 @@ safe_cast(json_value(item_list,'$.model_discounted_price') as float64) as model_
 safe_cast(json_value(item_list,'$.model_original_price') as float64) as model_original_price,
 safe_cast(json_value(item_list,'$.model_quantity_purchased') as int64) as model_quantity_purchased,
 json_value(item_list,'$.model_sku') as model_sku,
+COALESCE(regexp_extract(json_value(item_list,'$.model_sku'), r'[A-Z]{3}(2[1-5])'), "Cũ") AS year,
 json_value(item_list,'$.order_item_id') as order_item_id,
 json_value(item_list,'$.promotion_group_id') as promotion_group_id,
 json_value(item_list,'$.promotion_id') as promotion_id,
@@ -45,12 +46,16 @@ left join unnest(item_list) as item_list
 select 
     * except(promotion_type),
     regexp_extract(model_sku,r'-?(\w+)') as product_code,
-    COALESCE(
+    {# COALESCE(
         REGEXP_EXTRACT(
             regexp_extract(model_sku,r'-?(\w+)$'),
             r'(?:Y0|YY|00|YB)?([A-Z]{3}\d{5})'), 
             REGEXP_EXTRACT(model_sku,r'(?:Y0|YY|00|YB)?([A-Z]{3}\d{5})')
-            ) as class_code,
+            ) as class_code, #}
+    CASE
+        WHEN year = "24" THEN regexp_extract(model_sku, r"^(?:[ZXY0]{0,3})([BC0][A-Z]{3}[0-9]{3,5})")
+        else regexp_extract(model_sku, r"^(?:[ZXY0]{0,2})([BC0]?[A-Z]{3}[0-9]{3,5})") end
+    AS class_code,
     case  {% for k,v in promotion_types.items() -%}
     when promotion_type = "{{k}}" then "{{v}}"
     {% endfor -%}
