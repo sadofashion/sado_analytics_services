dbf069cfbc9693b84d70dbd759b757d866f5e3cf
Chien Le <lchien063@gmail.com>
2024-11-11 15:27:13 +0700

    feat: update models & source

--- snapshots/stg_gsheet__cogs.sql
+++ snapshots/stg_gsheet__cogs.sql
@@ -13,5 +13,7 @@
     product_code , 
     inventory_value_per_unit,
     from {{ source('gSheet', 'cogs') }}
+    where regexp_extract(_FILE_NAME,r'cogs__(\d{6})') = format_date('%Y%m',current_date)
+    
 
 {% endsnapshot %}
