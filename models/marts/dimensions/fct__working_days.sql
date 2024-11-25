ded1e3e668ed927e5cae47501d5d9febb3031e39
Chien Le <lchien063@gmail.com>
2024-11-25 15:53:31 +0700

    feat: add DX

--- models/marts/dimensions/fct__working_days.sql
+++ models/marts/dimensions/fct__working_days.sql
@@ -10,8 +10,9 @@ select
     c.* except(start_of_week,end_of_week),
     case when date_diff(c.date,b.opening_day,day) <= 10 then 'Khai trương' else p.promotion end as promotion,
 from {{ ref("dim__branches") }} b
-left join {{ ref("calendar") }} c on b.opening_day <= c.date and (b.close_date >= c.date or b.close_date is null)
+left join {{ ref("calendar") }} c on coalesce(b.opening_day,'2023-01-01') <= c.date and (b.close_date >= c.date or b.close_date is null)
 left join {{ ref("promotions") }} p on c.date>=p.start and c.date<=p.end and p.location = b.model_type
 where b.opening_day is not null
-and b.channel not in ('Kho & CH khác Kiotviet',"Điểm xả")
+and b.channel not in ('Kho & CH khác Kiotviet')
 and c.date <= current_date()
+{# and b.branch_id not in (1000087891) #}
