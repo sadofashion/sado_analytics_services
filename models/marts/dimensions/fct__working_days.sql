b82b7bdbd5c7ab444f9b39eecf67157b6f7d8193
Chien Le <lchien063@gmail.com>
2024-11-26 09:38:43 +0700

    feat: DX branches

--- models/marts/dimensions/fct__working_days.sql
+++ models/marts/dimensions/fct__working_days.sql
@@ -8,11 +8,14 @@ select
     {{dbt_utils.generate_surrogate_key(['b.branch_id', 'c.date'])}} as branch_working_day_id,
     b.* except(phone,email,province_code, region_code, frontage, area_sqm),
     c.* except(start_of_week,end_of_week),
-    case when date_diff(c.date,b.opening_day,day) <= 10 then 'Khai trương' else p.promotion end as promotion,
+    case when b.channel = 'Điểm xả' then b.channel
+    when date_diff(c.date,b.opening_day,day) <= 10 then 'Khai trương' 
+    else p.promotion end as promotion,
 from {{ ref("dim__branches") }} b
 left join {{ ref("calendar") }} c on coalesce(b.opening_day,'2023-01-01') <= c.date and (b.close_date >= c.date or b.close_date is null)
 left join {{ ref("promotions") }} p on c.date>=p.start and c.date<=p.end and p.location = b.model_type
-where b.opening_day is not null
+where 1=1
+{# and b.opening_day is not null #}
 and b.channel not in ('Kho & CH khác Kiotviet')
 and c.date <= current_date()
 {# and b.branch_id not in (1000087891) #}
