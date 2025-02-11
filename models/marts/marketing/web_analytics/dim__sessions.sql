0820b317a29efc9f363b67c833da29ceb3970494
Chien Le <lchien063@gmail.com>
2025-02-11 20:23:21 +0700

    feat: web metrics

--- models/marts/marketing/web_analytics/dim__sessions.sql
+++ models/marts/marketing/web_analytics/dim__sessions.sql
@@ -6,4 +6,10 @@
 SELECT
     *
 FROM
-    {{ ref("dim_ga4__sessions") }}
+    {{ ref("dim_ga4__sessions") }} s
+    where landing_page_hostname in (
+        '5sfashion.vn',
+        'www.5sfashion.vn',
+        'khaosat.5sfashion.vn',
+        'vqmm.5sfashion.com.vn'
+    )
